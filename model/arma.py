5db4f488a74a6259259995dc310078da89e51f42
Sandro Santana <osandro@protonmail.com>
2025-03-30 21:21:55 -0300

    shoot and recharge

--- model/arma.py
+++ model/arma.py
@@ -1,7 +1,25 @@
+from random import randint
+
 class Gun:
     def __init__(self):
-        self.__mag_limit = 6
-        self.__current_loaded = 0
+        self.__pente = []
     
-    def shoot(self):
-        print("bang")
+    @property
+    def pente(self):
+        """The pente property."""
+        return self.__pente
+
+    @pente.setter
+    def pente(self, value):
+        self.__pente = value
+    
+    def atirar(self):
+        bala = self.pente[0]
+        self.pente.pop(0)
+        return bala
+
+    def carregar(self):
+        for i in range(6):
+            result = randint(0,1)
+            self.pente.append(bool(result))
+
