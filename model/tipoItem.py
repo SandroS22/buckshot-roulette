db8b001b08cb9e9dc23e815e2db5f95045f31173
Sandro Santana <osandro@protonmail.com>
2025-04-02 10:31:59 -0300

    Add tipoItem

---
+++ model/tipoItem.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class TipoItem(Enum):
+    LUPA = "Olha a bala atual"
+    CERVEJA = "Retira uma bala do pente"
+    ALGEMA = "Prente o oponente por uma rodada"
+    CIGARRO = "Recupera 1 de vida"
+    SERRA = "Duplica o dano"
