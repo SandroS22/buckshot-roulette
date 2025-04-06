afb62243caa9e213819ddfca84399c2685842f28
Sandro Santana <osandro@protonmail.com>
2025-04-05 21:59:29 -0300

    Change itens list to player or oponent itens list

--- ui/interface.py
+++ ui/interface.py
@@ -3,12 +3,14 @@ from tkinter import Label, Frame, PhotoImage, Menu
 
 class Interface:
     def __init__(self):
-        self.__itens = ["algemas.png", "cerveja.png", "cigarro.png", "lupa.png", "serra.png"]
+        self.__itens = []
         self.__player_icone = "jogador.png"
         self.__root = tk.Tk()
         self.__status_bar = None
         self.__balas = []
         self.__msg = ""
+        self.__itens_player =[]
+        self.__itens_oponente = []
 
     @property
     def itens(self):
@@ -53,6 +55,22 @@ class Interface:
     @msg.setter
     def msg(self, value):
         self.__msg = value
+
+    @property
+    def itens_player(self):
+        return self.__itens_player
+
+    @itens_player.setter
+    def itens_player(self, value):
+        self.__itens_player = value
+
+    @property
+    def itens_oponente(self):
+        return self.__itens_oponente
+
+    @itens_oponente.setter
+    def itens_oponente(self, value):
+        self.__itens_oponente = value
 
     def on_icon_click(self, icon_name):
         print(f"Ícone clicado: {icon_name}")
@@ -102,46 +120,49 @@ class Interface:
             health_bar.pack()
 
     def criar_slots(self):
-        icons = [PhotoImage(file=item).subsample(15) for item in self.itens]
-        icon_index = 0  # para rastrear qual ícone estamos usando
+        # Duas listas apenas, como você quer
 
-        # Definindo as posições dos 4 quadrantes
-        quadrantes = [
-            (50, 50),  # Superior esquerdo
-            (600, 50),  # Superior direito
-            (50, 300),  # Inferior esquerdo
-            (600, 300)  # Inferior direito
-        ]
+        # Criar iteradores compartilhados
+        self.icon_iter_cima = iter([PhotoImage(file=item).subsample(15) for item in self.itens_oponente])
+        self.item_iter_cima = iter(self.itens_oponente)
 
-        for x_base, y_base in quadrantes:
-            for row in range(2):
-                for col in range(2):
-                    # Calcula a posição de cada slot dentro do quadrante
-                    x = x_base + col * 50
-                    y = y_base + row * 50
+        self.icon_iter_baixo = iter([PhotoImage(file=item).subsample(15) for item in self.itens_player])
+        self.item_iter_baixo = iter(self.itens_player)
 
-                    # Cria o frame do slot
-                    icon_frame = Frame(self.root, bg='gray')
-                    icon_frame.place(x=x, y=y, width=45, height=45)
+        # Criar os quadrantes de cima compartilhando os iteradores
+        self.criar_quadrante(50, 50, self.icon_iter_cima, self.item_iter_cima)
+        self.criar_quadrante(600, 50, self.icon_iter_cima, self.item_iter_cima)
 
-                    # Cria a borda branca
-                    border = Frame(icon_frame, bg='black', width=80, height=80)
-                    border.pack(padx=5, pady=5, fill='both', expand=True)
+        # Criar os quadrantes de baixo compartilhando os iteradores
+        self.criar_quadrante(50, 300, self.icon_iter_baixo, self.item_iter_baixo)
+        self.criar_quadrante(600, 300, self.icon_iter_baixo, self.item_iter_baixo)
 
-                    if icon_index < len(icons):
-                        icon_image = icons[icon_index]
-                        item_name = self.itens[icon_index]
+    def criar_quadrante(self, x_base, y_base, icon_iter, item_iter):
+        for row in range(2):
+            for col in range(2):
+                x = x_base + col * 50
+                y = y_base + row * 50
+                self.criar_slot(x, y, icon_iter, item_iter)
 
-                        label = Label(border, image=icon_image, bg='white', borderwidth=2, relief="solid")
-                        label.image = icon_image  # Por algum motivo o garbage colector some com o icone sem isso
-                        label.bind("<Button-1>", lambda e, name=item_name: self.nova_msg(name))
-                        label.pack(fill='both', expand=True)
+    def criar_slot(self, x, y, icon_iter, item_iter):
+        icon_frame = Frame(self.root, bg='gray')
+        icon_frame.place(x=x, y=y, width=50, height=50)
 
-                        icon_index += 1
-                    else:
-                        # Slot vazio, mas mantendo o espaço e borda
-                        empty_label = Label(border, bg='white', borderwidth=2, relief="solid")
-                        empty_label.pack(fill='both', expand=True)
+        border = Frame(icon_frame, bg='white', width=80, height=80)
+        border.pack(padx=5, pady=5, fill='both', expand=True)
+
+        try:
+            icon_image = next(icon_iter)
+            item_name = next(item_iter)
+
+            label = Label(border, image=icon_image, bg='white', borderwidth=2, relief="solid")
+            label.image = icon_image
+            label.bind("<Button-1>", lambda e, name=item_name: self.nova_msg(name))
+            label.pack(fill='both', expand=True)
+
+        except StopIteration:
+            empty_label = Label(border, bg='white', borderwidth=2, relief="solid")
+            empty_label.pack(fill='both', expand=True)
 
     def criar_mensagens(self):
         turn_label = Label(self.root, text=self.msg, bg='white', borderwidth=2, relief="solid")
@@ -176,8 +197,9 @@ class Interface:
         self.msg = msg
         self.atualizar_ui()
 
-    def adicionar_itens(self, itens):
-        self.itens.extend(itens)
+    def adicionar_itens(self, itens_player, itens_oponente):
+        self.itens_player = itens_player
+        self.itens_oponente = itens_oponente
         self.atualizar_ui()
 
 
