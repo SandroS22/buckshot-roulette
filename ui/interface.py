c7d74145b6c11c2d2acd22bc95dba45a5a1fedb1
Sandro Santana <osandro@protonmail.com>
2025-04-05 22:44:28 -0300

    Change healthbar to properly show player and oponent health

--- ui/interface.py
+++ ui/interface.py
@@ -11,6 +11,8 @@ class Interface:
         self.__msg = ""
         self.__itens_player =[]
         self.__itens_oponente = []
+        self.__vida_player = 6
+        self.__vida_oponente = 6
 
     @property
     def itens(self):
@@ -72,7 +74,28 @@ class Interface:
     def itens_oponente(self, value):
         self.__itens_oponente = value
 
+    @property
+    def vida_player(self):
+        return self.__vida_player
+
+    @vida_player.setter
+    def vida_player(self, value):
+        self.__vida_player = value
+
+    @property
+    def vida_oponente(self):
+        return self.__vida_oponente
+
+    @vida_oponente.setter
+    def vida_oponente(self, value):
+        self.__vida_oponente = value
+
+
     def on_icon_click(self, icon_name):
+        if icon_name == "Jogador 0":
+            icon_name = "Oponente"
+        if icon_name == "Jogador 1":
+            icon_name = "Player"
         print(f"Ícone clicado: {icon_name}")
 
     def criar_ui(self):
@@ -103,39 +126,54 @@ class Interface:
         menu = Menu(menubar, tearoff=False)
         menu.add_command(label="Conectar", command=self.conectar)
         menu.add_command(label="Desconectar", command=self.desconectar)
-        menu.add_command(label="Add", command=self.atualizar_ui)
         menubar.add_cascade(label="Menu", menu=menu)
 
     def criar_players(self):
         player_icon = PhotoImage(file=self.player_icone).subsample(8)
+
         for i, y in enumerate([50, 300]):
+            # Criar o frame do jogador
             player_frame = Frame(self.root, bg='gray')
             player_frame.place(x=300, y=y, width=200, height=100)
+
+            # Ícone do jogador
             player_label = Label(player_frame, image=player_icon, bg='gray')
-            player_label.image = player_icon # Por algum motivo o garbage colector some com o icone sem isso
+            player_label.image = player_icon  # Evita que o garbage collector remova a imagem
             player_label.bind("<Button-1>", lambda e, name=f"Jogador {i}": self.on_icon_click(name))
             player_label.pack()
 
-            health_bar = Frame(player_frame, bg='green', width=10, height=10)
-            health_bar.pack()
+            # Criar frame para a barra de vida
+            health_bar_frame = Frame(player_frame, bg='black')
+            health_bar_frame.pack(pady=5)
+
+            # Criar vidas dentro da barra
+            if y == 50:
+                for vida in range(self.vida_oponente):
+                    vida_frame = Frame(health_bar_frame, bg='green', width=10, height=10)
+                    vida_frame.grid(row=0, column=vida, padx=2, pady=2)
+            if y == 300:
+                for vida in range(self.vida_player):
+                    vida_frame = Frame(health_bar_frame, bg='green', width=10, height=10)
+                    vida_frame.grid(row=0, column=vida, padx=2, pady=2)
 
     def criar_slots(self):
-        # Duas listas apenas, como você quer
+        self.itens_player = ["lupa.png", "serra.png", "algemas.png", "cigarro.png", "serra.png", "algemas.png"]
+        self.itens_oponente = ["cigarro.png", "serra.png", "algemas.png"]
 
         # Criar iteradores compartilhados
-        self.icon_iter_cima = iter([PhotoImage(file=item).subsample(15) for item in self.itens_oponente])
-        self.item_iter_cima = iter(self.itens_oponente)
+        icon_iter_cima = iter([PhotoImage(file=item).subsample(15) for item in self.itens_oponente])
+        item_iter_cima = iter(self.itens_oponente)
 
-        self.icon_iter_baixo = iter([PhotoImage(file=item).subsample(15) for item in self.itens_player])
-        self.item_iter_baixo = iter(self.itens_player)
+        icon_iter_baixo = iter([PhotoImage(file=item).subsample(15) for item in self.itens_player])
+        item_iter_baixo = iter(self.itens_player)
 
         # Criar os quadrantes de cima compartilhando os iteradores
-        self.criar_quadrante(50, 50, self.icon_iter_cima, self.item_iter_cima)
-        self.criar_quadrante(600, 50, self.icon_iter_cima, self.item_iter_cima)
+        self.criar_quadrante(50, 50, icon_iter_cima, item_iter_cima)
+        self.criar_quadrante(600, 50, icon_iter_cima, item_iter_cima)
 
         # Criar os quadrantes de baixo compartilhando os iteradores
-        self.criar_quadrante(50, 300, self.icon_iter_baixo, self.item_iter_baixo)
-        self.criar_quadrante(600, 300, self.icon_iter_baixo, self.item_iter_baixo)
+        self.criar_quadrante(50, 300, icon_iter_baixo, item_iter_baixo)
+        self.criar_quadrante(600, 300, icon_iter_baixo, item_iter_baixo)
 
     def criar_quadrante(self, x_base, y_base, icon_iter, item_iter):
         for row in range(2):
@@ -177,10 +215,12 @@ class Interface:
             Label(self.status_bar, bg=color, width=2, height=1, borderwidth=2, relief="solid").pack()
 
     def conectar(self):
-        print("Conectando")
+        self.msg = "CONECTADO"
+        self.atualizar_ui()
 
     def desconectar(self):
-        print("Desconectando")
+        self.msg = "DESCONECTADO"
+        self.atualizar_ui()
 
     def atualizar_ui(self):
         for widget in self.root.winfo_children():
@@ -200,6 +240,11 @@ class Interface:
     def adicionar_itens(self, itens_player, itens_oponente):
         self.itens_player = itens_player
         self.itens_oponente = itens_oponente
+        self.atualizar_ui()
+
+    def alterar_vida(self, vida_player, vida_oponente):
+        self.vida_player = vida_player
+        self.vida_oponente = vida_oponente
         self.atualizar_ui()
 
 
