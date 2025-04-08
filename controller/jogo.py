9e655e577d95a6aa17666d3ef05a39949425bca5
Sandro Santana <osandro@protonmail.com>
2025-04-08 20:48:09 -0300

    Add match inicialization

--- controller/jogo.py
+++ controller/jogo.py
@@ -1,10 +1,32 @@
 from model.partida import Partida
-import tkinter as tk
+
+from ui.interface import Interface
 
 
 class JogoController:
     def __init__(self):
-        pass
-        
+        self.__interface = None
+        self.__partida = None
+
+    @property
+    def interface(self):
+        return self.__interface
+
+    @interface.setter
+    def interface(self, interface: Interface):
+        self.__interface = interface
+
+    @property
+    def partida(self):
+        return self.__partida
+
+    @partida.setter
+    def partida(self, value):
+        self.__partida = value
+
     def iniciar(self):
-        pass
+        self.interface = Interface()
+        self.partida = Partida(self.interface)
+        self.interface.conectar = self.partida.comecar_nova_partida
+        self.interface.desconectar = self.partida.comecar_nova_partida
+        self.interface.criar_ui()
--- model/partida.py
+++ model/partida.py
@@ -1,10 +1,15 @@
+from dog.dog_actor import DogActor
 from model.arma import Arma
+from ui.interface import Interface
+
 
 class Partida:
-    def __init__(self):
+    def __init__(self, interface: Interface):
         self.__arma = Arma()
         self.__partida_em_andamento = False
         self.__jogadores = []
+        self.__dog_server = DogActor()
+        self.__interface = interface
 
     @property
     def arma(self):
@@ -26,6 +31,22 @@ class Partida:
     def jogadores(self):
         return self.__jogadores
 
+    @property
+    def dog_server(self):
+        return self.__dog_server
+
+    @dog_server.setter
+    def dog_server(self, value):
+        self.__dog_server = value
+
+    @property
+    def interface(self):
+        return self.__interface
+
+    @interface.setter
+    def interface(self, value):
+        self.__interface = value
+
 
     def checa_ganhador(self):
         if self.partida_em_andamento:
@@ -33,4 +54,17 @@ class Partida:
                 if jogador.venceu:
                     return jogador
         return False
+
+    def comecar_nova_partida(self):
+        if self.partida_em_andamento:
+            return
+
+        start_status = self.dog_server.start_match(2)
+        code = start_status.get_code()
+        message = start_status.get_message()
+
+        if code == "0" or code == "1":
+            self.interface.nova_msg(message)
+        else:
+            self.interface.nova_msg(message)
 
