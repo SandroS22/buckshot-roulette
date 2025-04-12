a9caf8c114a9572ec2213d889834bd1a227ca2a3
Sandro Santana <osandro@protonmail.com>
2025-04-12 00:13:07 -0300

    Add connection to dog server

--- model/partida.py
+++ model/partida.py
@@ -1,15 +1,27 @@
+from time import sleep, time
 from dog.dog_actor import DogActor
+from dog.dog_interface import DogPlayerInterface
 from model.arma import Arma
+from model.jogador import Jogador
 from ui.interface import Interface
+from tkinter import simpledialog, messagebox
 
 
-class Partida:
+class Partida(DogPlayerInterface):
     def __init__(self, interface: Interface):
+        super().__init__()
         self.__arma = Arma()
-        self.__partida_em_andamento = False
-        self.__jogadores = []
-        self.__dog_server = DogActor()
+        self.__status_partida = 1
+        self.__player_local = Jogador()
+        self.__player_remoto = Jogador()
         self.__interface = interface
+        player_name = simpledialog.askstring(title="Player identification", prompt="Qual o seu nome?")
+        self.__dog_server = DogActor()
+        message = self.__dog_server.initialize(player_name, self)
+        messagebox.showinfo(message=message)
+        self.__interface.atualizar_ui()
+
+
 
     @property
     def arma(self):
@@ -20,16 +32,28 @@ class Partida:
         self.__arma = value
 
     @property
-    def partida_em_andamento(self):
-        return self.__partida_em_andamento
+    def status_partida(self):
+        return self.__status_partida
 
-    @partida_em_andamento.setter
-    def partida_em_andamento(self, value):
-        self.__partida_em_andamento = value
+    @status_partida.setter
+    def status_partida(self, value):
+        self.__status_partida = value
 
     @property
-    def jogadores(self):
-        return self.__jogadores
+    def player_local(self):
+        return self.__player_local
+
+    @player_local.setter
+    def player_local(self, value):
+        self.__player_local = value
+
+    @property
+    def player_remoto(self):
+        return self.__player_remoto
+
+    @player_remoto.setter
+    def player_remoto(self, value):
+        self.__player_remoto = value
 
     @property
     def dog_server(self):
@@ -49,14 +73,17 @@ class Partida:
 
 
     def checa_ganhador(self):
-        if self.partida_em_andamento:
-            for jogador in self.jogadores:
-                if jogador.venceu:
-                    return jogador
-        return False
+        if self.player_local.venceu:
+            return self.player_local
+        elif self.player_remoto.venceu:
+            return self.player_remoto
+        return None
 
-    def comecar_nova_partida(self):
-        if self.partida_em_andamento:
+    def desconectar(self):
+        pass
+
+    def comecar_nova_partida_command(self):
+        if self.status_partida == "3" or self.status_partida == "4" or self.status_partida == "5":
             return
 
         start_status = self.dog_server.start_match(2)
@@ -66,5 +93,19 @@ class Partida:
         if code == "0" or code == "1":
             self.interface.nova_msg(message)
         else:
-            self.interface.nova_msg(message)
+            self.comecar_partida(start_status.get_players())
+            self.interface.atualizar_ui()
+
+    def comecar_partida(self, players):
+        player_local = players[0]
+        player_remoto = players[1]
+        print(players)
+
+        self.player_local = self.player_local.iniciar_player(player_local)
+        self.player_remoto = self.player_remoto.iniciar_player(player_remoto)
+        
+        if self.player_local.is_turno:
+            self.status_partida = "3"
+        elif self.player_remoto.is_turno:
+            self.status_partida = "5"
 
